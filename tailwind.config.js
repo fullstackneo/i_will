bdd10e705bed94e5ce84f3135e84c48047352989
Neo <fullstackneo@gmail.com>
2022-11-23 23:11:21 -0700

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,13 @@ module.exports = {
     }
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
     extend: {
       fontFamily: {
         fa: 'FontAwesome',
